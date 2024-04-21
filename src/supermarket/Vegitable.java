c905f2c7dfdc54b04b29ebcdd1a55fb4162e7b97
Jun Sasaki <iajunsasaki@gmail.com>
2024-04-22 08:31:31 +0900

    野菜クラス追加

---
+++ src/supermarket/Vegitable.java
@@ -0,0 +1,15 @@
+package supermarket;
+
+/**
+ * 野菜クラス。
+ */
+public class Vegitable extends Product {
+	/**
+	 * コンストラクタ。
+	 * @param name 商品名
+	 * @param unitPrice 税抜単価
+	 */
+	public Vegitable(String name, int unitPrice) {
+		super(name, "生鮮食品", unitPrice);
+	}
+}
